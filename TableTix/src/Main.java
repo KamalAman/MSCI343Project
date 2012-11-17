ef00b5eff6953b55147672d13e6036df8509302c
Kamal Aman <kamalmkaman@gmail.com>
2012-11-17 15:54:47 -0500

    Main Implementation

---
+++ TableTix/src/Main.java
@@ -0,0 +1,18 @@
+
+import processing.core.*;
+
+public class Main extends PApplet 
+{
+
+
+	  public void setup() 
+	  {
+	    size(600,600);
+
+	  }
+
+	  public void draw() 
+	  {
+	    background(0);
+	  }
+	}
