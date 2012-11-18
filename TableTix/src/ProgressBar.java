64fe67577e71d1a809e1a9bcb21c6c19012aa872
AndreaLam <andrea.justine.lam@gmail.com>
2012-11-17 19:49:46 -0500

    Progress bar

--- TableTix/src/ProgressBar.java
+++ TableTix/src/ProgressBar.java
@@ -3,8 +3,33 @@ import vialab.SMT.*;
 
 public class ProgressBar {
 	
-	PImage screen1 = Global.applet.loadImage("...//Image//Progress Bar//Screen 1.png");
-	PImage screen2 = Global.applet.loadImage("...//Image//Progress Bar//Screen 2.png");
-	PImage screen3 = Global.applet.loadImage("...//Image//Progress Bar//Screen 3.png");
-	PImage screen4 = Global.applet.loadImage("...//Image//Progress Bar//Screen 4.png");
+	 PImage screen1 = Global.applet.loadImage("...//Image//Progress Bar//Screen 1.png");
+	 PImage screen2 = Global.applet.loadImage("...//Image//Progress Bar//Screen 2.png");
+	 PImage screen3 = Global.applet.loadImage("...//Image//Progress Bar//Screen 3.png");
+	 PImage screen4 = Global.applet.loadImage("...//Image//Progress Bar//Screen 4.png");
+	 
+	 public void SetProgressBar()
+	 {
+		 if (Global.progressBar != null)
+		 {
+			 Global.client.remove(Global.progressBar);
+		 }
+		 
+		 switch (Global.currentScreen) 
+		 {
+			case 1:
+				Global.progressBar = new ImageZone(screen1, 0, 0);
+				break;
+			case 2: 
+				Global.progressBar = new ImageZone(screen2, 0, 0);
+				break;
+			case 3: 
+				Global.progressBar = new ImageZone(screen3, 0, 0);
+				break;
+			case 4:
+				Global.progressBar = new ImageZone(screen4, 0, 0);
+				break;
+		}
+		 Global.client.add(Global.progressBar);
+	 }	
 }
